aab94f095c95498fc4fa3e15c11ecce51f331bcc
Bradley Selee <bselee415@gmail.com>
2024-07-06 21:16:07 -0400

    Understanding sar error

--- scripts/defocus_sicd.py
+++ scripts/defocus_sicd.py
@@ -58,35 +58,58 @@ def main(base_config_path: str):
     chip_names = sicd_csv["chip_name"].tolist()
     apply_full_path = lambda file, root: f"{root}/{file}.npy"
 
-    full_chip_path = [apply_full_path(f_name, chips_root_path) for f_name in chip_names]
+    full_chip_paths = [apply_full_path(f_name, chips_root_path) for f_name in chip_names]
 
-    #start here@@@@@@@@@@@@@@@@@@@@@@
 
     assert sicd_paths
 
     _, sicd_reader = load_sicd_pixels(sicd_paths[0])
 
-    sicd_pixels = np.load(chip_paths[5])
+    sicd_pixels = np.load(full_chip_paths[5])
 
-    # Degrade image with random 10th order polynomial phase
+    # Degrade image with random 9th order polynomial phase
     breakpoint()
-    coeff = (np.random.rand(10) - 0.5) * sicd_pixels.shape[0]
+    # Generate 10 random coefficients between [-0.5,0.5] and multiply by azimuth length; 
+    # for a 256x256 chip, this will generate coefficients between [-128, 128]
+    coeff = (np.random.rand(3) - 0.5) * sicd_pixels.shape[0]
+    
+    # Generate sequential x data points between [-1,1] for each azimuth index
     x = np.linspace(-1, 1, sicd_pixels.shape[0])
+    
+    # Evaluate the nth order polynomial at each x point
     y = np.poly1d(coeff)(x)
+    
+    # Create a line from the data using  least-squares regression to remove the linear trend;
+    # I'm not sure why we need to remove the linear trend 
     slope, intercept, r_value, p_value, std_err = linregress(x, y)
-    line = slope * x + np.mean(y)
+    line = slope * x + np.mean(y) # np.mean(y) is the same as intercept in this case; this is only true when the data is mean-centered i.e. subtracting the y mean from y and x mean from x
     y = y - line
-    ph_err = np.tile(np.array([y]).T, (1, sicd_pixels.shape[1]))
+    
+    # Repeat the azimuth phase error y across the range bins
+    # Example: y = [[1],    ph_err = [[1, 1, 1, ..., range_len]
+    #               [2],              [2, 2, 2, ..., range_len]
+    #               [3]]              [3, 3, 3, ..., range_len]]
+    ph_err = np.tile(y[:, np.newaxis], (1, sicd_pixels.shape[1]))
+    
+    # Apply phase error by taking the inverse
     img_err = ft(ift(sicd_pixels, ax=0) * np.exp(1j * ph_err), ax=0)
+    img_err2 = ift(ft(sicd_pixels, ax=0) * np.exp(1j * ph_err), ax=0)
 
     visualizer = Visualizer()
 
     # Initialize remapper with data_mean from full scene SICD
     remapper = Density()
     remapper.calculate_global_parameters_from_reader(sicd_reader)
+    breakpoint()
+    
+    ################# START HERE, go through https://github.com/antsfamily/torchbox/tree/main/torchbox tb.fft and see how they do the phase correction
+    ################# next conver this to azimuth phase error and see if that makes a difference
 
-    save_name = "defocused.png"
+    save_name = "defocused2.png"
+    save_name_test = "defocused2_test.png"
     visualizer.plot_sicd(complex_pixels=img_err, remapper=remapper, save_path=save_name)
+    visualizer.plot_sicd(complex_pixels=img_err2, remapper=remapper, save_path=save_name_test)
+    visualizer.plot_sicd(complex_pixels=sicd_pixels, remapper=remapper, save_path="orignal.png")
     print("Finished?")
 
 
